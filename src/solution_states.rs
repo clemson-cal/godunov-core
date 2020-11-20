03c50839f814aa449febbd955403849196bc2aa3
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-11-20 11:42:17 -0500

    Add a solution state struct for ArcArray

--- src/solution_states.rs
+++ src/solution_states.rs
@@ -1,5 +1,5 @@
 use core::ops::{Add, Mul};
-use ndarray::{Array, Dimension};
+use ndarray::{Array, ArcArray, Dimension};
 use num::rational::Rational64;
 use num::ToPrimitive;
 
@@ -39,6 +39,46 @@ impl<T, D: Dimension> Mul<Rational64> for SolutionStateArray<T, D> where Array<T
             time: self.time * b.to_f64().unwrap(),
             iteration: self.iteration * b,
             conserved: self.conserved * b.to_f64().unwrap(),
+        }
+    }
+}
+
+
+
+
+//============================================================================
+#[derive(Clone)]
+pub struct SolutionStateArcArray<T, D: Dimension>
+{
+    pub time: f64,
+    pub iteration: Rational64,
+    pub conserved: ArcArray<T, D>,
+}
+
+impl<T, D: Dimension> Add for SolutionStateArcArray<T, D> where ArcArray<T, D>: Add<ArcArray<T, D>, Output=ArcArray<T, D>>, T: Clone
+{
+    type Output = SolutionStateArcArray<T, D>;
+
+    fn add(self, b: Self::Output) -> Self::Output
+    {
+        Self::Output{
+            time: self.time + b.time,
+            iteration: self.iteration + b.iteration,
+            conserved:(self.conserved + b.conserved).to_shared(),
+        }
+    }
+}
+
+impl<T, D: Dimension> Mul<Rational64> for SolutionStateArcArray<T, D> where ArcArray<T, D>: Mul<f64, Output=ArcArray<T, D>>, T: Clone
+{
+    type Output = SolutionStateArcArray<T, D>;
+
+    fn mul(self, b: Rational64) -> Self::Output
+    {
+        Self::Output{
+            time: self.time * b.to_f64().unwrap(),
+            iteration: self.iteration * b,
+            conserved:(self.conserved * b.to_f64().unwrap()).to_shared(),
         }
     }
 }
