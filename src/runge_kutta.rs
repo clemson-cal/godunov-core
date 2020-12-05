67b8d0ee07e96a878d9188f2a9a57dec11687e1f
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-12-05 09:53:19 -0500

    Add WeightedAverage trait for use by RK

--- src/runge_kutta.rs
+++ src/runge_kutta.rs
@@ -22,29 +22,46 @@ impl std::error::Error for InvalidRungeKuttaOrder {}
 
 
 
-//============================================================================
-fn advance_rk1<State, Update>(s0: State, update: Update) -> State where State: Clone + Add<Output=State> + Mul<Rational64, Output=State>, Update: Fn(State) -> State
+// ============================================================================
+pub trait WeightedAverage
+{
+    fn weighted_average(self, br: Rational64, s0: &Self) -> Self;
+}
+
+impl<S> WeightedAverage for S where S: Clone + Add<Output=S> + Mul<Rational64, Output=S>
+{
+    fn weighted_average(self, b: Rational64, s0: &Self) -> Self
+    {
+        self * (-b + 1) + s0.clone() * b
+    }
+}
+
+
+
+
+// ============================================================================
+pub fn advance_rk1<S: WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S
 {
     update(s0)
 }
 
-fn advance_rk2<State, Update>(s0: State, update: Update) -> State where State: Clone + Add<Output=State> + Mul<Rational64, Output=State>, Update: Fn(State) -> State
+pub fn advance_rk2<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S
 {
     let b1 = Rational64::new(1, 2);
-    let mut s1 = s0.clone();
-    s1 =           update(s1);
-    s1 = s0 * b1 + update(s1) * (-b1 + 1);
+    let s1 = s0.clone();
+    let s1 = update(s1);
+    let s1 = update(s1).weighted_average(b1, &s0);
     s1
 }
 
-fn advance_rk3<State, Update>(s0: State, update: Update) -> State where State: Clone + Add<Output=State> + Mul<Rational64, Output=State>, Update: Fn(State) -> State
+pub fn advance_rk3<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S
 {
     let b1 = Rational64::new(3, 4);
     let b2 = Rational64::new(1, 3);
-    let mut s1 = s0.clone();
-    s1 =                   update(s1);
-    s1 = s0.clone() * b1 + update(s1) * (-b1 + 1);
-    s1 = s0.clone() * b2 + update(s1) * (-b2 + 1);
+    let s1 = s0.clone();
+    let s1 = update(s1);
+    let s1 = update(s1).weighted_average(b1, &s0);
+    let s1 = update(s1).weighted_average(b2, &s0);
     s1
 }
 
@@ -84,7 +101,10 @@ impl TryFrom<i64> for RungeKuttaOrder
 //============================================================================
 impl RungeKuttaOrder
 {
-    pub fn advance<State, Update>(self, state: State, update: Update) -> State where State: Clone + Add<Output=State> + Mul<Rational64, Output=State>, Update: Fn(State) -> State
+    pub fn advance<State, Update>(self, state: State, update: Update) -> State
+    where
+        State: Clone + WeightedAverage,
+        Update: Fn(State) -> State
     {
         match self {
             RungeKuttaOrder::RK1 => advance_rk1(state, update),        
