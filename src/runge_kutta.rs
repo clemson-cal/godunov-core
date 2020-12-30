db3fd7a7480343ae0a49d100454900da802eb4e3
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-12-30 17:50:28 -0500

    Add optional serde dependency to load validated RK order

--- src/runge_kutta.rs
+++ src/runge_kutta.rs
@@ -10,10 +10,8 @@ use num::rational::Rational64;
 #[derive(Debug)]
 pub struct InvalidRungeKuttaOrder {}
 
-impl std::fmt::Display for InvalidRungeKuttaOrder
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result
-    {
+impl std::fmt::Display for InvalidRungeKuttaOrder {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "Runge-Kutta order must be 1, 2, or 3")
     }
 }
@@ -24,15 +22,12 @@ impl std::error::Error for InvalidRungeKuttaOrder {}
 
 
 // ============================================================================
-pub trait WeightedAverage
-{
+pub trait WeightedAverage {
     fn weighted_average(self, br: Rational64, s0: &Self) -> Self;
 }
 
-impl<S> WeightedAverage for S where S: Clone + Add<Output=S> + Mul<Rational64, Output=S>
-{
-    fn weighted_average(self, b: Rational64, s0: &Self) -> Self
-    {
+impl<S> WeightedAverage for S where S: Clone + Add<Output=S> + Mul<Rational64, Output=S> {
+    fn weighted_average(self, b: Rational64, s0: &Self) -> Self {
         self * (-b + 1) + s0.clone() * b
     }
 }
@@ -41,13 +36,11 @@ impl<S> WeightedAverage for S where S: Clone + Add<Output=S> + Mul<Rational64, O
 
 
 // ============================================================================
-pub fn advance_rk1<S: WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S
-{
+pub fn advance_rk1<S: WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S {
     update(s0)
 }
 
-pub fn advance_rk2<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S
-{
+pub fn advance_rk2<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S {
     let b1 = Rational64::new(1, 2);
     let s1 = s0.clone();
     let s1 = update(s1);
@@ -55,8 +48,7 @@ pub fn advance_rk2<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update
     s1
 }
 
-pub fn advance_rk3<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S
-{
+pub fn advance_rk3<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update: Update) -> S {
     let b1 = Rational64::new(3, 4);
     let b2 = Rational64::new(1, 3);
     let s1 = s0.clone();
@@ -71,8 +63,7 @@ pub fn advance_rk3<S: Clone + WeightedAverage, Update: Fn(S) -> S>(s0: S, update
 
 // ============================================================================
 #[async_trait::async_trait]
-pub trait WeightedAverageAsync: Clone
-{
+pub trait WeightedAverageAsync: Clone {
     type Runtime;
     async fn weighted_average(self, b: Rational64, s0: &Self, runtime: &Self::Runtime) -> Self;
 }
@@ -85,8 +76,7 @@ pub async fn advance_async_rk1<S, R, U, F>(state: S, update: U, _runtime: &R) ->
     where
     S: WeightedAverageAsync<Runtime=R>,
     U: Fn(S) -> F,
-    F: Future<Output=S>,
-{
+    F: Future<Output=S> {
     update(state).await
 }
 
@@ -94,8 +84,7 @@ pub async fn advance_async_rk2<S, R, U, F>(state: S, update: U, runtime: &R) -> 
     where
     S: WeightedAverageAsync<Runtime=R>,
     U: Fn(S) -> F,
-    F: Future<Output=S>,
-{
+    F: Future<Output=S> {
     let b1 = Rational64::new(1, 2);
 
     let s1 = state.clone();
@@ -108,8 +97,7 @@ pub async fn advance_async_rk3<S, R, U, F>(state: S, update: U, runtime: &R) -> 
     where
     S: WeightedAverageAsync<Runtime=R>,
     U: Fn(S) -> F,
-    F: Future<Output=S>,
-{
+    F: Future<Output=S> {
     let b1 = Rational64::new(3, 4);
     let b2 = Rational64::new(1, 3);
 
@@ -125,8 +113,8 @@ pub async fn advance_async_rk3<S, R, U, F>(state: S, update: U, runtime: &R) -> 
 
 //============================================================================
 #[derive(Clone, Copy, Debug)]
-pub enum RungeKuttaOrder
-{
+#[cfg_attr(feature="serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum RungeKuttaOrder {
     RK1,
     RK2,
     RK3,
@@ -136,11 +124,11 @@ pub enum RungeKuttaOrder
 
 
 //============================================================================
-impl TryFrom<i64> for RungeKuttaOrder
-{
+impl TryFrom<i64> for RungeKuttaOrder {
+
     type Error = InvalidRungeKuttaOrder;
-    fn try_from(order: i64) -> Result<RungeKuttaOrder, Self::Error>
-    {
+
+    fn try_from(order: i64) -> Result<RungeKuttaOrder, Self::Error> {
         match order {
             1 => Ok(RungeKuttaOrder::RK1),
             2 => Ok(RungeKuttaOrder::RK2),
@@ -154,13 +142,11 @@ impl TryFrom<i64> for RungeKuttaOrder
 
 
 //============================================================================
-impl RungeKuttaOrder
-{
+impl RungeKuttaOrder {
     pub fn advance<State, Update>(self, state: State, update: Update) -> State
     where
         State: Clone + WeightedAverage,
-        Update: Fn(State) -> State
-    {
+        Update: Fn(State) -> State {
         match self {
             RungeKuttaOrder::RK1 => advance_rk1(state, update),
             RungeKuttaOrder::RK2 => advance_rk2(state, update),
@@ -169,11 +155,10 @@ impl RungeKuttaOrder
     }
 
     pub async fn advance_async<S, R, U, F>(self, state: S, update: U, runtime: &R) -> S
-        where
+    where
         S: WeightedAverageAsync<Runtime=R>,
         U: Fn(S) -> F,
-        F: Future<Output=S>,
-    {
+        F: Future<Output=S> {
         match self {
             RungeKuttaOrder::RK1 => advance_async_rk1(state, update, runtime).await,
             RungeKuttaOrder::RK2 => advance_async_rk2(state, update, runtime).await,
