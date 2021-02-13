d8dd524ad7bb0e0a83f976a1a3cca335e3432b07
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-02-13 12:08:13 -0500

    Add try_advance_async family of RK functions

--- src/runge_kutta.rs
+++ src/runge_kutta.rs
@@ -72,19 +72,44 @@ pub trait WeightedAverageAsync: Clone {
 
 
 // ============================================================================
+pub async fn try_advance_async_rk1<S, R, U, F, E>(state: S, update: U, _runtime: &R) -> Result<S, E>
+where
+    S: WeightedAverageAsync<Runtime=R>,
+    U: Fn(S) -> F,
+    F: Future<Output=Result<S, E>>
+{
+    update(state).await
+}
+
 pub async fn advance_async_rk1<S, R, U, F>(state: S, update: U, _runtime: &R) -> S
     where
     S: WeightedAverageAsync<Runtime=R>,
     U: Fn(S) -> F,
-    F: Future<Output=S> {
+    F: Future<Output=S>
+{
     update(state).await
 }
 
-pub async fn advance_async_rk2<S, R, U, F>(state: S, update: U, runtime: &R) -> S
-    where
+pub async fn try_advance_async_rk2<S, R, U, F, E>(state: S, update: U, runtime: &R) -> Result<S, E>
+where
     S: WeightedAverageAsync<Runtime=R>,
     U: Fn(S) -> F,
-    F: Future<Output=S> {
+    F: Future<Output=Result<S, E>>
+{
+    let b1 = Rational64::new(1, 2);
+
+    let s1 = state.clone();
+    let s1 = update(s1).await?;
+    let s1 = update(s1).await?.weighted_average(b1, &state, runtime).await;
+    Ok(s1)
+}
+
+pub async fn advance_async_rk2<S, R, U, F>(state: S, update: U, runtime: &R) -> S
+where
+    S: WeightedAverageAsync<Runtime=R>,
+    U: Fn(S) -> F,
+    F: Future<Output=S>
+{
     let b1 = Rational64::new(1, 2);
 
     let s1 = state.clone();
@@ -93,11 +118,28 @@ pub async fn advance_async_rk2<S, R, U, F>(state: S, update: U, runtime: &R) -> 
     s1
 }
 
-pub async fn advance_async_rk3<S, R, U, F>(state: S, update: U, runtime: &R) -> S
-    where
+pub async fn try_advance_async_rk3<S, R, U, F, E>(state: S, update: U, runtime: &R) -> Result<S, E>
+where
     S: WeightedAverageAsync<Runtime=R>,
     U: Fn(S) -> F,
-    F: Future<Output=S> {
+    F: Future<Output=Result<S, E>>
+{
+    let b1 = Rational64::new(3, 4);
+    let b2 = Rational64::new(1, 3);
+
+    let s1 = state.clone();
+    let s1 = update(s1).await?;
+    let s1 = update(s1).await?.weighted_average(b1, &state, runtime).await;
+    let s1 = update(s1).await?.weighted_average(b2, &state, runtime).await;
+    Ok(s1)
+}
+
+pub async fn advance_async_rk3<S, R, U, F>(state: S, update: U, runtime: &R) -> S
+where
+    S: WeightedAverageAsync<Runtime=R>,
+    U: Fn(S) -> F,
+    F: Future<Output=S>
+{
     let b1 = Rational64::new(3, 4);
     let b2 = Rational64::new(1, 3);
 
@@ -146,7 +188,8 @@ impl RungeKuttaOrder {
     pub fn advance<State, Update>(self, state: State, update: Update) -> State
     where
         State: Clone + WeightedAverage,
-        Update: Fn(State) -> State {
+        Update: Fn(State) -> State
+    {
         match self {
             RungeKuttaOrder::RK1 => advance_rk1(state, update),
             RungeKuttaOrder::RK2 => advance_rk2(state, update),
@@ -158,11 +201,25 @@ impl RungeKuttaOrder {
     where
         S: WeightedAverageAsync<Runtime=R>,
         U: Fn(S) -> F,
-        F: Future<Output=S> {
+        F: Future<Output=S>
+    {
         match self {
             RungeKuttaOrder::RK1 => advance_async_rk1(state, update, runtime).await,
             RungeKuttaOrder::RK2 => advance_async_rk2(state, update, runtime).await,
             RungeKuttaOrder::RK3 => advance_async_rk3(state, update, runtime).await,
+        }
+    }
+
+    pub async fn try_advance_async<S, R, U, F, E>(self, state: S, update: U, runtime: &R) -> Result<S, E>
+    where
+        S: WeightedAverageAsync<Runtime=R>,
+        U: Fn(S) -> F,
+        F: Future<Output=Result<S, E>>
+    {
+        match self {
+            RungeKuttaOrder::RK1 => try_advance_async_rk1(state, update, runtime).await,
+            RungeKuttaOrder::RK2 => try_advance_async_rk2(state, update, runtime).await,
+            RungeKuttaOrder::RK3 => try_advance_async_rk3(state, update, runtime).await,
         }
     }
 }
